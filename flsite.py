0e064adc1a0bce1425795476afc17a661e8fa4c6
slimen1985 <vadart1985@gmail.com>
2023-01-12 14:43:18 +0300

    lesson2-4

--- flsite.py
+++ flsite.py
@@ -1,19 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
-
-menu = ["Установка", "Первое приложение", "Обратная связь"]
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}]
 
 
 @app.route("/")
 def index():
+    print(url_for('index'))
     return render_template('index.html', menu=menu)
 
 
 @app.route("/about")
 def about():
+    print(url_for('about'))
     return render_template('about.html', title="О сайте", menu=menu)
+
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        print(request.form)
+    return render_template('contact.html', title="Обратная связь", menu=menu)
+
+
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
