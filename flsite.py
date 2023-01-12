a9f600daf0f50c77b3532de098954545cc289b0e
slimen1985 <vadart1985@gmail.com>
2023-01-12 16:37:34 +0300

    lesson5-6

--- flsite.py
+++ flsite.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash, redirect, session, abort
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'safdafgsd4456656fsdf54d'
+
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
         {"name": "Обратная связь", "url": "contact"}]
@@ -21,10 +23,33 @@ def about():
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
     if request.method == "POST":
-        print(request.form)
+        if len(request.form['username']) > 2:
+            flash("Сообщение отправлено", category='success')
+        else:
+            flash("Ошибка отправки", category='error')
     return render_template('contact.html', title="Обратная связь", menu=menu)
 
 
+@app.route("/profile/<username>")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+    return f"Профиль пользователя {username}"
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == "vadart" and request.form['psw'] == "123":
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="Авторизация", menu=menu)
+
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('page404.html', title="Страница не найдена", menu=menu)
 
 
 if __name__ == "__main__":
